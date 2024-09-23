0cfc1d3e9d2a76d21558f0a11c875a834d7eaaae
Robert Trifan <trifangrobert@gmail.com>
2024-09-24 01:11:37 +0300

    Initial commit for gh-pages branch

---
+++ README.md
@@ -0,0 +1,1 @@
+# trifangrobert.github.io
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainContent from './components/MainContent';
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
